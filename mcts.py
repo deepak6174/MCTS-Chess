40315932c616bea9d66fd16934c59d026d8b77c0
Deepak Kumar Singh <edeepak.singh@outlook.com>
2023-08-10 15:14:55 +0100

    first

---
+++ mcts.py
@@ -0,0 +1,121 @@
+from __future__ import division
+from __future__ import print_function
+from ctypes import *
+import time
+import math
+import random
+
+nnue = cdll.LoadLibrary("./libnnueprobe.so")
+
+nnue.nnue_init(b'nn-62ef826d1a6d.nnue')
+
+
+def randomPolicy(state):
+    score = nnue.nnue_evaluate_fen(bytes(state.board.fen(), encoding='utf-8'))
+    return score
+
+
+class treeNode():
+    def __init__(self, state, parent):
+        self.state = state
+        self.is_terminal = self.state.is_terminal()
+        self.isFullyExpanded = self.is_terminal
+        self.parent = parent
+        self.numVisits = 0
+        self.totalReward = 0
+        self.children = {}
+
+    def __str__(self):
+        s=[]
+        s.append("totalReward: %s"%(self.totalReward))
+        s.append("numVisits: %d"%(self.numVisits))
+        s.append("is_terminal: %s"%(self.is_terminal))
+        s.append("possibleActions: %s"%(self.children.keys()))
+        return "%s: {%s}"%(self.__class__.__name__, ', '.join(s))
+
+class mcts():
+    def __init__(self, timeLimit=None, iterationLimit=None, explorationConstant=1 / math.sqrt(2),
+                 rolloutPolicy=randomPolicy):
+        if timeLimit != None:
+            if iterationLimit != None:
+                raise ValueError("Cannot have both a time limit and an iteration limit")
+            self.timeLimit = timeLimit
+            self.limitType = 'time'
+        else:
+            if iterationLimit == None:
+                raise ValueError("Must have either a time limit or an iteration limit")
+            if iterationLimit < 1:
+                raise ValueError("Iteration limit must be greater than one")
+            self.searchLimit = iterationLimit
+            self.limitType = 'iterations'
+        self.explorationConstant = explorationConstant
+        self.rollout = rolloutPolicy
+
+    def search(self, initialState, needDetails=False):
+        self.root = treeNode(initialState, None)
+        reward = 0
+        if self.limitType == 'time':
+            timeLimit = time.time() + self.timeLimit / 1000
+            while time.time() < timeLimit:
+                self.executeRound()
+        else:
+            for i in range(self.searchLimit):
+                self.executeRound()
+
+        bestChild = self.getBestChild(self.root, 0)
+        action=(action for action, node in self.root.children.items() if node is bestChild).__next__()
+        if needDetails:
+            return {"action": action, "expectedReward": bestChild.totalReward / bestChild.numVisits}
+        else:
+            return action, 
+
+    def executeRound(self):
+        node = self.selectNode(self.root)
+        reward = self.rollout(node.state)
+        self.backpropogate(node, reward)
+
+    def selectNode(self, node):
+        while not node.is_terminal:
+            if node.isFullyExpanded:
+                node = self.getBestChild(node, self.explorationConstant)
+            else:
+                return self.expand(node)
+        return node
+
+    def expand(self, node):
+        actions = node.state.generate_states()
+        for action in actions:
+            if action not in node.children:
+                newNode = treeNode(node.state.takeAction(action), node)
+                node.children[action] = newNode
+                if len(actions) == len(node.children):
+                    node.isFullyExpanded = True
+                return newNode
+
+        raise Exception("Should never reach here")
+
+    def backpropogate(self, node, reward):
+        turn = -1
+        while node is not None:
+            node.numVisits += 1
+            node.totalReward += reward * turn
+            node = node.parent
+            turn *= -1
+
+    def getBestChild(self, node, explorationValue):
+        bestValue = float("-inf")
+        bestNodes = []
+        for child in node.children.values():
+            nodeValue = child.totalReward / child.numVisits + explorationValue * math.sqrt(
+                2 * math.log(node.numVisits) / child.numVisits)
+            
+            # if explorationValue == 0:
+            #     print('totol score: ' , child.totalReward/child.numVisits)
+            #     print(child.numVisits)
+            #     print(child.state.board)
+            if nodeValue > bestValue:
+                bestValue = nodeValue
+                bestNodes = [child]
+            elif nodeValue == bestValue:
+                bestNodes.append(child)
+        return random.choice(bestNodes)
---
+++ state.py
@@ -0,0 +1,43 @@
+from copy import deepcopy
+from mcts import mcts as MCTS
+import chess
+
+
+class State:
+    def __init__(self, opponent=False):
+        self.board = chess.Board()
+        if opponent:
+            self.board = deepcopy(opponent.board)
+
+    def is_terminal(self):
+        return self.board.is_game_over()
+    
+    def generate_states(self):
+        states = []
+        moves = self.board.legal_moves
+        for move in moves:
+            states.append(str(move))
+        return states
+    
+    def takeAction(self, move):
+        new_state = State(self)
+        new_state.board.push(chess.Move.from_uci(move))
+        return new_state
+    
+    def game_loop(self):
+
+        mcts = MCTS(timeLimit=1000)
+
+        while True:
+            best_move = mcts.search(self)
+            self = self.takeAction(best_move)
+            print(self)
+            print("Best Move: ", best_move)
+    def __str__(self):
+        return self.board.unicode().replace('⭘', '.')
+    
+
+
+
+
+
